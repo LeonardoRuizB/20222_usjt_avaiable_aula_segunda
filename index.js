ac3b24b9a6a1dd71cbc216b95cb4a1d78d626ba9
LeonardoRuizB <leobottura12@gmail.com>
2022-08-30 21:28:21 -0300

    Fizemos muitas coisas, estou enviando o Commit para deixar salvo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const axios = require ('axios')
 const appid = process.env.appid
 
-const q = 'Itu'
+const q = 'Sao Paulo'
 
 //Metric celsius
 //Imperail fazhnheit
@@ -16,9 +16,40 @@ const cnt = 10
 const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
 
 const url = `${base_url}?q=${q}&units=${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
+
 const p = axios.get(url)
+
 p
 .then((res) => {
   console.log(res.data)
   return res.data
 })
+
+axios
+    .get(url)
+    .then((res) => {
+        //Mostra o resultado e devolve somente a parte de interesse
+        console.log(res)
+        return res.data
+    })
+    .then((res) => {
+        //mostra o total e devolve o resultado
+        console.log(res.cnt)
+        return res
+    })
+    .then((res) => {
+        //devolve somente a lista de previsões
+        console.log("aqui", res)
+        return res['list']
+    })
+    .then((res) => {
+        //para cada resultado, mostra algumas informações
+        for(let previsao of res)
+        console.log(`
+            ${new Date(previsao.dt * 1000).toLocaleString()},
+            ${'Min: ' + previsao.main.temp_min}\u00B0C,
+            ${'Max: ' + previsao.main.temp_max}\u00B0C,
+            ${'Umd: ' + previsao.main.humidity}%
+            ${previsao.weather[0].description}
+        `)
+    })
